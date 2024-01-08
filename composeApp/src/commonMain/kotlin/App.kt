3b5581629b6b2e3e3fe3920e24bdaf39c68bf007
Andy Zhu <jinczhu@microsoft.com>
2024-01-08 17:23:08 +0800

    Add auto darkmode code.

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,4 +1,6 @@
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
@@ -14,16 +16,19 @@ import com.arkivanov.decompose.extensions.compose.subscribeAsState
 @Composable
 fun App(root: RootComponent) {
     MaterialTheme(
-        colorScheme = lightColorScheme()
-    ) {
+//        colorScheme = if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme()
+        colorScheme = lightColorScheme(),
+        ) {
         val childStack by root.childStack.subscribeAsState()
-        Children(
-            stack = childStack,
-            animation = stackAnimation(slide()),
-        ) { child ->
-            when(val instance = child.instance) {
-                is RootComponent.Child.LevelSelectScreen -> LevelSelectScreen(instance.component)
-                is RootComponent.Child.MainGameScreen -> MainGameScreen(instance.component)
+        Surface {
+            Children(
+                stack = childStack,
+                animation = stackAnimation(slide()),
+                ) { child ->
+                when(val instance = child.instance) {
+                    is RootComponent.Child.LevelSelectScreen -> LevelSelectScreen(instance.component)
+                    is RootComponent.Child.MainGameScreen -> MainGameScreen(instance.component)
+                }
             }
         }
     }
